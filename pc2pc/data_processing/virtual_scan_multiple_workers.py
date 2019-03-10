e7c1c444b5b0c4356b3eb85362b0e6db5c67c014
ChenXuelinCXL <xuelin.chen.sdu@gmail.com>
2019-03-10 16:35:36 +0000

    regu bk

--- pc2pc/data_processing/virtual_scan_multiple_workers.py
+++ pc2pc/data_processing/virtual_scan_multiple_workers.py
@@ -3,5 +3,6 @@ import os
 num_workers = 8
 
 for i in range(0,num_workers):
-    a = 'nohup python3 virtual_scan_shapenet_objects.py --worker_id %d --num_workers %d > log%d.txt 2>&1 &' % (i, num_workers, i)
+    #a = 'nohup python3 virtual_scan_shapenet_objects.py --worker_id %d --num_workers %d > log%d.txt 2>&1 &' % (i, num_workers, i)
+    a = 'nohup python3 virtual_scan_shapenet-v1_objects.py --worker_id %d --num_workers %d > log%d.txt 2>&1 &' % (i, num_workers, i)
     os.system(a)

707c1ceb1a3de700c39aed25edb0c062bd8197ef
ChenXuelinCXL <xuelin.chen.sdu@gmail.com>
2019-03-12 15:03:05 +0000

    regular backup

--- pc2pc/train_pcl2pcl_gan_3D-EPN_GT.py
+++ pc2pc/train_pcl2pcl_gan_3D-EPN_GT.py
@@ -24,7 +24,7 @@ from latent_gan import PCL2PCLGAN
 import shapenet_pc_dataset
 import config
 
-cat_name = 'table'
+cat_name = 'car'
 loss = 'emd' # using emd loss against gt
 
 para_config_gan = {

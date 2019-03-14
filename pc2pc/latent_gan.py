dcf2f14d74ea4517e2772973cd7ebfc9d086bc87
ChenXuelinCXL <xuelin.chen.sdu@gmail.com>
2019-03-14 17:57:21 +0000

    RBK

--- pc2pc/latent_gan.py
+++ pc2pc/latent_gan.py
@@ -193,7 +193,7 @@ class PCL2PCLGAN:
         fake_clean_reconstr = self.clean_decoder(self.fake_code, tf.constant(False, shape=()))
 
         reconstr_loss = self._reconstruction_loss(fake_clean_reconstr, self.input_noisy_cloud) # comput loss against the input
-        #G_tofool_loss = self._generator_loss(self.D, self.fake_code)
+        G_tofool_loss = self._generator_loss(self.D, self.fake_code)
         #G_loss = G_tofool_loss + self.para_config['lambda'] * reconstr_loss
         G_loss = self.para_config['lambda'] * reconstr_loss # no GAN loss
 
@@ -204,7 +204,7 @@ class PCL2PCLGAN:
         # eval only loss
         eval_loss = self._reconstruction_loss(fake_clean_reconstr, self.gt, eval_loss=self.para_config['eval_loss'])
 
-        return G_loss, G_loss, reconstr_loss, D_loss, D_fake_loss, D_real_loss, fake_clean_reconstr, eval_loss
+        return G_loss, G_tofool_loss, reconstr_loss, D_loss, D_fake_loss, D_real_loss, fake_clean_reconstr, eval_loss
     
 
     def _reconstruction_loss(self, recon, input, eval_loss=None):
